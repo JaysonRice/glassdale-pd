cea5b6445c4018a99ddf96973e7e5c01816ab762
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-09 13:46:02 -0500

    Added criminal card layout and basic styling elements.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 import { getOfficers } from "./officers/OfficerProvider.js";
 import { getCriminals } from "./criminals/CriminalProvider.js";
-import { getCrimes } from "./convictions/CrimeProvider.js";
+import { getConvictions } from "./convictions/ConvictionProvider.js";
 import CriminalList from "./criminals/CriminalList.js";
 
 
 getOfficers()
-getCrimes()
+getConvictions()
 getCriminals().then(criminal => CriminalList())
