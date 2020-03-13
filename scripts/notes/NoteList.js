39890c7a74a3077a8aeda72a97e7f2bbd44e9f93
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-13 12:00:57 -0500

    Added basis for saving notes.

---
+++ scripts/notes/NoteList.js
@@ -0,0 +1,20 @@
+//finish the code
+
+
+// Handle browser-generated click event in component
+contentTarget.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+
+        // Make a new object representation of a note
+        const newNote = {
+            // Key/value pairs here
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+    }
+})
+
+const NoteForm = () => {
+    // rest of the code here
+}
