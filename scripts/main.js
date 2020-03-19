2a0ce47b12e57c24de204dddfc33f28df58e7b92
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-19 14:29:04 -0400

    Criminals now show a button that brings up a dialog with their associates shown.

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,11 @@ import { getCriminals } from "./criminals/CriminalProvider.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { getConvictions } from "./convictions/ConvictionProvider.js"
 import ConvictionSelect from "./convictions/ConvictionSelect.js"
-import NoteForm from "./notes/NoteForm.js"
 import { DisplayNotesButton } from "./notes/DisplayNotesButton.js"
+import NoteForm from "./notes/NoteForm.js"
 import "./notes/NotesList.js"
+import "./criminals/KnownAssociatesDialog.js"
 import { DisplayNoteFormButton } from "./notes/DisplayNoteFormButton.js"
-
 getCriminals().then(CriminalList)
 
 // first get all convictions, THEN create the conviction dropdown
